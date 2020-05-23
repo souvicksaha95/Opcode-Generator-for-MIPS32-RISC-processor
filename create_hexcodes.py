55cd2516775c4622f356bc499f8914b3b7db13f3
Souvick Saha <souvicksaha95@gmail.com>
2020-05-23 20:19:12 +0530

    Update create_hexcodes.py

--- create_hexcodes.py
+++ create_hexcodes.py
@@ -5,3 +5,6 @@ result_binary = '0'
 result_binary = result_binary.rjust(32, '0')
 with open('C:/Users/Souvick/Desktop/opcode.json') as f:
     data = json.load(f)
+while (1):
+    command_input = input("Enter: ")
+    command = re.split(',\\ |\\ ',command_input)
